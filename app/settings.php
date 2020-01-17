e883cf5440892a18569a8eaeaad1933e26a13fd5
David Deizepi <david_deizepi@hotmail.com>
2020-01-17 18:54:12 -0300

    Initial commit - só foi o README ¬¬'

---
+++ app/settings.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use DI\ContainerBuilder;
+use Monolog\Logger;
+
+return function (ContainerBuilder $containerBuilder) {
+    // Global Settings Object
+    $containerBuilder->addDefinitions([
+        'settings' => [
+            'displayErrorDetails' => true, // Should be set to false in production
+            'db' => [
+                'driver'    => 'mysql',
+                'host'      => 'localhost',
+                'database'  => 'database',
+                'username'  => 'root',
+                'password'  => '',
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix'    => '',
+            ],
+            'logger' => [
+                'name' => 'slim-app',
+                'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+                'level' => Logger::DEBUG,
+            ],
+        ],
+    ]);
+};
